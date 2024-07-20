c6a08334ef819d1652bd2e3ff8f988af42b1b2b8
9309480340 <kjadhav67844@gmail.com>
2024-07-20 19:05:11 +0530

    Add files via upload

---
+++ gr.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number::"))
+b=int(input("enter the second number::"))
+c=int(input("enter the third number::"))
+if(a>b and a>c):
+    print("a is grater than b and c")
+elif(b>a and b>c):
+    print("b is grater than a and c")
+else:
+    print("c is grater than a and b")
